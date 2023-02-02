1cc6b70a8ace796632091d8771d7170660f5c19c
Justin Song <121644228+Jung-GunSong@users.noreply.github.com>
2023-02-02 15:31:10 -0500

    Update submission.js

--- submission.js
+++ submission.js
@@ -1,10 +1,30 @@
 const findSum = function(array) {
   // your code here - don't forget to return a number!
+  let result =0;
+  for (let element of array){
+    result += element;
+    return result;
 };
 
 const findFrequency = function(array) {
   // your code here - don't forget to return an object!
-};
+  let countingObj = {};
+  for (let element of array){
+    if (countingObj[element] === undefined){
+      countingObj[element] = 1;
+    } else{
+      countingObj[element] += 1;
+    }
+   let countingObjKeys = Object.keys(countingObj);
+   let coutingObjValues = Object.values(countingObj);
+   let mostKey = null;
+   let leastKey = null;
+   let mostValue = coutingObjValues.reduce((function (accumulator, currentValue){
+      if (currentValue > accumulator){
+        accumulator = currentValue;
+      }
+    
+},0);
 
 const isPalindrome = function(str) {
   // your code here - don't forget to return a boolean!
